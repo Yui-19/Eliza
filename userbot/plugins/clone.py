3c0152c012b31ab2358f0ef78404c60866c0ec04
Yui-19 <108167384+Yui-19@users.noreply.github.com>
2022-06-27 15:18:35 +0530

    Update clone.py

--- userbot/plugins/clone.py
+++ userbot/plugins/clone.py
@@ -91,7 +91,7 @@ async def _(event):
     await event.client(functions.account.UpdateProfileRequest(about=bio))
     await event.client(functions.account.UpdateProfileRequest(first_name=name))
     await event.client(functions.account.UpdateProfileRequest(last_name=blank))
-    await edit_delete(event, "successfully reverted to your account back")
+    await edit_delete(event, "Successfully reverted to your account back")
     if BOTLOG:
         await event.client.send_message(
             BOTLOG_CHATID,
