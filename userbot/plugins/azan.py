3967faa240ae6c39890aebe08c12a3f04b1e1803
Yui-19 <108167384+Yui-19@users.noreply.github.com>
2022-06-27 13:04:29 +0530

    Update azan.py

--- userbot/plugins/azan.py
+++ userbot/plugins/azan.py
@@ -15,13 +15,13 @@ plugin_category = "extra"
     command=("azan", plugin_category),
     info={
         "header": "Shows you the islamic prayer times of the given city name",
-        "note": "you can set default city by using {tr} setcity command",
+        "note": "You can set default city by using {tr} setcity command",
         "usage": "{tr}azan <city name>",
         "examples": "{tr}azan hyderabad",
     },
 )
 async def get_adzan(adzan):
-    "Shows you the Islamic prayer times of the given city name"
+    "Shows you the islamic prayer times of the given city name"
     input_str = adzan.pattern_match.group(1)
     LOKASI = gvarstatus("DEFCITY") or "Delhi" if not input_str else input_str
     url = f"http://muslimsalat.com/{LOKASI}.json?key=bd099c5825cbedb9aa934e255a81a5fc"
@@ -32,14 +32,14 @@ async def get_adzan(adzan):
         )
     result = json.loads(request.text)
     catresult = f"<b>Islamic prayer times </b>\
-            \n\n<b>City     : </b><i>{result['query']}</i>\
-            \n<b>Country  : </b><i>{result['country']}</i>\
-            \n<b>Date     : </b><i>{result['items'][0]['date_for']}</i>\
-            \n<b>Fajr     : </b><i>{result['items'][0]['fajr']}</i>\
-            \n<b>Shurooq    : </b><i>{result['items'][0]['shurooq']}</i>\
-            \n<b>Dhuhr    : </b><i>{result['items'][0]['dhuhr']}</i>\
-            \n<b>Asr    : </b><i>{result['items'][0]['asr']}</i>\
-            \n<b>Maghrib    : </b><i>{result['items'][0]['maghrib']}</i>\
-            \n<b>Isha     : </b><i>{result['items'][0]['isha']}</i>\
+            \n\n<b>City : </b><i>{result['query']}</i>\
+            \n<b>Country : </b><i>{result['country']}</i>\
+            \n<b>Date : </b><i>{result['items'][0]['date_for']}</i>\
+            \n<b>Fajr : </b><i>{result['items'][0]['fajr']}</i>\
+            \n<b>Shurooq : </b><i>{result['items'][0]['shurooq']}</i>\
+            \n<b>Dhuhr : </b><i>{result['items'][0]['dhuhr']}</i>\
+            \n<b>Asr : </b><i>{result['items'][0]['asr']}</i>\
+            \n<b>Maghrib : </b><i>{result['items'][0]['maghrib']}</i>\
+            \n<b>Isha : </b><i>{result['items'][0]['isha']}</i>\
     "
     await edit_or_reply(adzan, catresult, "html")
