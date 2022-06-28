d2cd2fa51c4cc7a0ef0c9ecfb59ca3407ce494bf
Yui-19 <108167384+Yui-19@users.noreply.github.com>
2022-06-28 19:33:44 +0530

    Update managers.py

--- userbot/core/managers.py
+++ userbot/core/managers.py
@@ -37,7 +37,7 @@ async def edit_or_reply(
     if not noformat:
         text = md_to_text(text)
     if aslink or deflink:
-        linktext = linktext or "Message was to big so pasted to bin"
+        linktext = linktext or "Message was too large so pasted to bin"
         response = await paste_message(text, pastetype="s")
         text = linktext + f" [here]({response})"
         if event.sender_id in sudo_users:
