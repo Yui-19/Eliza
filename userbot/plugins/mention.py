f3314781864608cf7d96dac3e57e6265842247e2
Yui-19 <108167384+Yui-19@users.noreply.github.com>
2022-06-28 11:04:57 +0530

    Update mention.py

--- userbot/plugins/mention.py
+++ userbot/plugins/mention.py
@@ -61,7 +61,7 @@ async def _(event):
             "{tr}men username/userid text",
             "text (username/mention)[custom text] text",
         ],
-        "examples": ["{tr}men @mrconfused hi", "Hi @mrconfused[How are you?]"],
+        "examples": ["{tr}men @mrconfused hi", "hi @mrconfused[how are you?]"],
     },
 )
 async def _(event):
