b18d201e66c488a50d7193c7e32fb2c00eb0c244
Yui-19 <108167384+Yui-19@users.noreply.github.com>
2022-06-28 18:44:41 +0530

    Update troll.py

--- userbot/assistant/troll.py
+++ userbot/assistant/troll.py
@@ -18,13 +18,13 @@ async def on_plug_in_callback_query_handler(event):
             ids = [userid]
             if event.query.user_id in ids:
                 reply_pop_up_alert = (
-                    "You are not allowed to see this message, better luck next time!"
+                    "You are not allowed to see this message\n\nBetter luck next time"
                 )
             else:
                 encrypted_tcxt = message["text"]
                 reply_pop_up_alert = encrypted_tcxt
         except KeyError:
-            reply_pop_up_alert = "This message no longer exists in catub server"
+            reply_pop_up_alert = "This message no longer exists in cat userbot server"
     else:
-        reply_pop_up_alert = "This message no longer exists "
+        reply_pop_up_alert = "This message no longer exists"
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
