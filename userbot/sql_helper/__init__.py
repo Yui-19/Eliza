055128d157dd1f03c069728d5afb20094ae21ec7
Yui-19 <108167384+Yui-19@users.noreply.github.com>
2022-06-28 20:23:48 +0530

    Update __init__.py

--- userbot/sql_helper/__init__.py
+++ userbot/sql_helper/__init__.py
@@ -29,6 +29,6 @@ try:
 except AttributeError as e:
     # this is a dirty way for the work-around required for #23
     LOGS.error(
-        "DB_URI is not configured. Features depending on the database might have issues."
+        "DB_URI is not configured\n\nFeatures depending on the database might have issues"
     )
     LOGS.error(str(e))
